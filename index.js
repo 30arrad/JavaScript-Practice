653d4edaa4fe15fccc0ae5a6dff70d821f78982d
Arrad Ahmed <110816803+30arrad@users.noreply.github.com>
2024-10-20 15:37:58 +0600

    variables & Data Types
    Study And Practice

---
+++ index.js
@@ -0,0 +1,57 @@
+// console.log("Hello World");
+// console.log("Arrad Ahmed");
+
+// name ="Arrad";
+// fullName = "Arrad Ahmed";
+// age= 21;
+
+// console.log(age);
+
+// let name = "Arrad";
+// let fullName = "Arrad Ahmed";
+// let age = 21;
+// age = 23;
+// age = 24;
+
+// console.log(age); 
+//output 24
+
+// var name = "Arrad";
+// var fullName = "Arrad Ahmed";
+// var age = 21; 
+// age = 22; 
+// age = 23; 
+// console.log(name); 
+//var used na kora
+
+// const name = "Arrad";
+// const fullName = "Arrad Ahmed";
+// const age = 21;
+
+// console.log(name); 
+//const used kore variable update kora jay na.
+
+// const student = {
+//     name: "Arrad",
+//     fullName: "Arrad Ahmed",
+//     age: 21,
+//     address: "Dhaka",
+// }
+
+// student["age"] =23;
+// update kora
+
+// console.log(student);
+// console.log(student.address);
+// object make
+
+//Q1. Create a conts object called "product" to store information shown in the example below.
+
+const product = {
+    name: "Laptop",
+    price: 30000,
+    brand: "Lenovo",
+    color: "silver"
+} 
+
+console.log(product);
