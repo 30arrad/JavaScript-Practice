402206f4a503daf81932a56d2c402883e6c44b03
Arrad Ahmed <110816803+30arrad@users.noreply.github.com>
2024-10-28 11:42:34 +0600

    DOM
    part 3

--- DOM - Document Object Model/DOM.js
+++ DOM - Document Object Model/DOM.js
@@ -86,3 +86,28 @@ console.log("DOM - Document Object Model");
 // Delete Element
 
 // Node.remove (el); // remove the node
+
+
+// ----------------------------Qustion Slove ----------------
+
+// Q1: Create a new button element. Give it a text "click me", banground color of red & text color of white.
+
+// Insert the button as the first element inside the body tag.
+
+let newBtn = document.createElement("button");
+newBtn.innerText = "click me";
+newBtn.style.color ='white';
+newBtn.style.backgroundColor ='red';
+
+document.querySelector("body").prepend(newBtn);
+
+
+
+
+// Q2: Create a <p> tag in html, it a class some styling.
+// Now create a new class in css and try to append this class to the <p> element.
+
+// Did you notice, how you overwrite the class name when you add a new one? Solve this problem using classList.
+
+let para = document.querySelector("p");
+para.classList.add ("newPara");
