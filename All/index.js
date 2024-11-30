40a926f55347cf98a78b8d9e298ee5018e4de2e3
Arrad Ahmed <110816803+30arrad@users.noreply.github.com>
2024-11-30 18:50:43 +0600

    add

---
+++ All/index.js
@@ -0,0 +1,48 @@
+console.log("Hello World");
+
+let s = 100;
+
+if (s>=80){
+    console.log("A");
+}
+else if(s>=70){
+    console.log("B");
+}
+else if(s>=60){
+    console.log("C");
+}
+else if(s>=50){
+    console.log("D");
+}
+else{
+    console.log("F");
+}
+
+//Ternary Operator;
+
+let age = 15;
+let isMarried = (age >= 18)? "is Married" : "Not Married";
+
+console.log(isMarried);
+
+// for loop
+
+for (let i = 0; i < 5; i++) {
+    console.log(`number ${i}`);
+}
+
+//while loop
+
+let j =5;
+while (j < 10) {
+    console.log(`number ${j}`);
+    j++;
+}
+
+//do while loop
+
+let k = 10;
+do {
+    console.log(`number ${k}`);
+    k++;    
+} while (k < 15);
