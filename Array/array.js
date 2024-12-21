954da255845802e672fa63d62d1318946d6aceb5
Arrad Ahmed <110816803+30arrad@users.noreply.github.com>
2024-12-21 13:33:51 +0600

    Array

--- Array/array.js
+++ Array/array.js
@@ -54,3 +54,55 @@ const Squar = num.map((num) => num ** 2);
 const odd = num.filter((num) => num % 2 === 1);
 
 // console.log(odd);
+
+let sum = [1, 2, 3].reduce((a, b) =>a + b, 0);
+// console.log(sum); // Output: 6
+
+let arr = [1, 2, 3];
+arr.pop();
+// console.log(arr); // Output: [1, 2]
+arr.pop();
+// console.log(arr); // Output: [1]
+arr.pop();
+// console.log(arr); // Output: []
+
+// console.log([1, 29, 3, 4, 50, 6].lastIndexOf(50)); // Output: 2
+
+// [1, 2, 3, 4, 5 , 6].forEach(num => console.log(num));
+
+let ar = [1, 2, 3];
+ar.shift();
+// console.log(ar); // Output: [2, 3]
+
+// console.log([1, 2, 3].includes(3)); // Output: true
+
+// console.log([1, 2, 4].some(num => num > 3)); // Output: true
+
+let nums = [1, 2, 4];
+// console.log(nums.some(num => num > 3));
+
+let arr1 = [2, 3];
+arr1.unshift(1);
+// console.log(arr1); // Output: [1, 2, 3]
+
+// console.log([1, 2, 3].find(num => num > 2)); // Output: 3
+
+// console.log([1, 2, 3].every(num => num > 1));
+
+// console.log([1, 2].concat([3, 4])); // Output: [1, 2, 3, 4]
+
+// console.log([1, 1, 3].findIndex(num => num > 1)); // Output: 2
+
+// console.log([3, 1, 2, 4,7,6,7,9].sort()); // Output: [1, 2, 3, 4, 6, 7, 7, 9]
+
+// console.log([1, 2, 3, 5].slice(1)); // Output: [2, 3]
+
+// console.log([1, 2, 3, 0, 10, 4].filter(num => num > 1)); // Output: [2, 3, 10, 4]
+
+// console.log([1, 2, 3].join("*")); // Output: "1*2*3"
+
+let arr2 = [1, 2, 3];
+arr2.splice(1, 1, 4);
+console.log(arr2); // Output: [1, 4, 3]
+
+console.log([1, 2, 3].map(num => num * 2)); // Output: [2, 4, 6]
